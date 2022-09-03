9e454b6c54cee9041bddd885dbf36674a0048e5a
hedon954 <171725713@qq.com>
2022-09-03 14:53:38 +0800

    adds defer to close resoureces

--- tcp/server.go
+++ tcp/server.go
@@ -2,9 +2,14 @@ package tcp
 
 import (
 	"context"
+	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+
 	"go-tcp-server/interface/tcp"
 	"go-tcp-server/lib/logger"
-	"net"
 )
 
 type Config struct {
@@ -19,12 +24,31 @@ func ListenAndServeWithSignal(cfg *Config, handler tcp.Handler) error {
 	logger.Info("start to listen")
 
 	closeChan := make(chan struct{})
+	go handleSystemSignal(closeChan)
+
 	ListenAndServe(listener, handler, closeChan)
 
 	return nil
 }
 
 func ListenAndServe(listener net.Listener, handler tcp.Handler, closeChan <-chan struct{}) {
+
+	// system force exit
+	go func() {
+		<-closeChan
+		logger.Info("shutting down")
+		_ = listener.Close()
+		_ = handler.Close()
+	}()
+
+	// close listener and handler
+	defer func() {
+		_ = listener.Close()
+		_ = handler.Close()
+	}()
+
+	// records how many connections right now
+	waitDone := sync.WaitGroup{}
 
 	ctx := context.Background()
 	for {
@@ -33,8 +57,32 @@ func ListenAndServe(listener net.Listener, handler tcp.Handler, closeChan <-chan
 			break
 		}
 		logger.Info("accepted connection")
+		// add a new connection
+		waitDone.Add(1)
 		go func() {
+			// handle return, means current connections closed
+			defer waitDone.Done()
 			handler.Handle(ctx, conn)
 		}()
+	}
+	// wait for all connections to close
+	waitDone.Wait()
+}
+
+func handleSystemSignal(closeChan chan struct{}) {
+	sigChan := make(chan os.Signal)
+
+	// see https://blog.csdn.net/secretii/article/details/118342752
+	// SIGHUP hand up
+	// SIGQUIT error
+	// SIGTERM kill
+	// SIGINT ctrl + c
+	// SIGKILL kill -9
+	signal.Notify(sigChan, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT, syscall.SIGKILL)
+
+	sig := <-sigChan
+	switch sig {
+	case syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGHUP, syscall.SIGINT, syscall.SIGKILL:
+		closeChan <- struct{}{}
 	}
 }
